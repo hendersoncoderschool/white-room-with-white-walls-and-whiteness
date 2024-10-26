795393a079f1610747ee620be476e359f3d6e8e1
Henderson The Coder School <caleb.scott0125@gmail.com>
2024-10-26 12:00:11 -0700

    raycast interact cheese

--- Assets/Cam.cs
+++ Assets/Cam.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
+    public float raycastDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float vertical = Input.GetAxis("Mouse Y");
-        transform.Rotate(Vector3.right, -vertical * 10f * Time.deltaTime);
+        transform.Rotate(Vector3.right, -vertical * 100f * Time.deltaTime);
+
+        if (Input.GetKeyDown("e"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance))
+            {
+                if (hit.collider.CompareTag("Cheese"))
+                    Debug.Log("interacted with " + hit.collider.gameObject.name);
+            }
+        }
     }
 }
